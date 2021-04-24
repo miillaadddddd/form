a6445eaac5cbf47a225661cc86a4e09084652529
miillaadddddd <milad7212@gmail.com>
2021-04-24 17:18:28 +0430

    Done Rtl

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React from 'react';
+import { create } from 'jss';
+import rtl from 'jss-rtl';
+import { StylesProvider, jssPreset } from '@material-ui/core/styles';
+import createMuiTheme from './assests/theme/ThemeRtl.js';
+import { ThemeProvider } from '@material-ui/core/styles';
+
+// Configure JSS
+const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
 
 
 
 function App() {
   return (
+    <ThemeProvider theme={createMuiTheme}>
+    <StylesProvider jss={jss}>
     <div className="App">
-      
+       راست چین شدد
     </div>
+    </StylesProvider>
+    </ThemeProvider>
   );
 }
 
